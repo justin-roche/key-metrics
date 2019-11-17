667b6356a4b18083b6dcf770e6f2e1190c6bca81
Justin Roche <jproche5@gmail.com>
2019-11-17 21:46:24 +0700

    add metrics for tracking hours at desk/typing

--- src/key_metrics/core.clj
+++ src/key_metrics/core.clj
@@ -4,57 +4,92 @@
 
 (def log-path "/Users/justin/logfile.txt")
 (def log-file-delimiter "::")
-(def hour-keys 3500)
-(def day-hours (* hour-keys 8))
+(def keys-per-hour 4000)
+
+(def desk-key-interval 100)
+;; how close (in seconds) should two keys be for you to be considered "at your desk"
+
+(def typing-key-interval 5)
+;; how close (in seconds) should two keys be for you to be considered typing
+
+(def keys-per-day (* keys-per-hour 8))
 
 (defn get-formatter [s]
   (java.time.format.DateTimeFormatter/ofPattern s))
+
+(defn get-epoch [ldt]
+  (.toEpochSecond (.atZone  ldt (java.time.ZoneId/systemDefault))))
 
 (defn parse-date [d]
   (let [formatter  (get-formatter "E MMM dd HH:mm:ss yyyy")
         ldt  (java.time.LocalDateTime/parse d formatter)]
     {:obj ldt
+     :epoch (get-epoch ldt)
      :hour (.getHour ldt)}))
 
 (defn add-line [l]
   ;; parse a single line of the log file; parse individual keys to remove surrounding elements
   (let [entry  (->> (re-pattern log-file-delimiter)
-                (str/split l)
-                (map str/trim))]
-    (vector (str/replace (first entry) #"\[|\]" "")
-            (parse-date (last entry)))))
+                    (str/split l)
+                    (map str/trim))]
+    {:key (str/replace (first entry) #"\[|\]" "")
+     :time (parse-date (last entry))}))
 
 (defn read-file []
   (with-open [rdr (clojure.java.io/reader log-path)]
     (doall (map add-line (filter #(> (count %) 0) (line-seq rdr))))))
 
-(defn part-hour [els]
-  (partition-by #(:hour (last %)) els))
+(defn part-hour [keys]
+  (partition-by #(:hour (:time %)) keys))
 
 (defn hourly-report [hour-collection]
-  {:hour (:hour (last (last hour-collection)))
+  {:hour (:hour (:time (last hour-collection)))
    :count (count hour-collection)})
 
-(defn get-hours-worked [day]
-  (int (/ (count day) hour-keys)))
+(defn get-epoch-difference [a b]
+
+  (- (:epoch (:time a))
+     (:epoch (:time b))))
+
+(defn second-to-hours [s]
+  ;; (println "ms" ms)
+  ( / (/ s 60) 60))
+
+(defn get-clock-hours [day interval]
+  (let [t (subvec day 0)
+        key-interval interval]
+    (second-to-hours (loop [i 1 c 0]
+                       (if (= i (count t))
+                         c
+                         (let [dif  (get-epoch-difference
+                                     (nth t i)
+                                     (nth t (dec i)))
+                               added-interval (if (< dif key-interval) dif 0)]
+                           (do  (recur (inc i) (+ c added-interval)))))))))
+
+(defn get-key-hours [day]
+  ;; get the number of work hours per day based on estimated keys per work hour
+  (int (/ (count day) keys-per-hour)))
 
 (defn get-percent-for-day [d]
-  (int (* 100 (/ (count d) day-hours))))
+  (int (* 100 (/ (count d) keys-per-day))))
 
-(defn print-report [days]
-  (let [today (last days)
-        table [{:name  "total days" :value (count days)}
-               {:name  "keys today" :value (count today)}
-               {:name  "percent today" :value (get-percent-for-day today)}
-               {:name  "hours today" :value (get-hours-worked today)}
-               {:name  "keys this hour" :value (count (last (part-hour today)))}]]
+(defn print-today-report [days]
+  (let [today (vec (last days))
+        part (part-hour today)
+        table [;; {:name  "total days" :value (count days)}
+               {:name  "keys " :value (str (count today) "/" keys-per-day)}
+               {:name  "percent keys " :value (get-percent-for-day today)}
+               {:name  "key hours " :value (get-key-hours today)}
+               {:name  "typing hours " :value (format "%.2f" (double (get-clock-hours today typing-key-interval)))}
+               {:name  "desk hours " :value (format "%.2f" (double (get-clock-hours today desk-key-interval)))}
+               {:name  "keys this hour" :value (count (last part))}]]
     (pp/print-table table)))
 
 (defn get-frequencies [data]
   (let [table-data (->> (map first data)
                         frequencies
                         (sort-by last)
-                        ;; reverse
                         vec)
         table (map  (fn [el]
                       {:key (first el)
@@ -62,9 +97,8 @@
     (pp/print-table table)))
 
 (defn get-report [data]
-  (println "getting report")
-  (let [days  (partition-by #(.getDayOfWeek (:obj (last %)))  data)]
-    (print-report days)))
+  (let [days  (partition-by #(.getDayOfWeek (:obj (:time %)))  data)]
+    (print-today-report days)))
   ;; (get-frequencies data)
 
 (defn set-interval [callback ms]

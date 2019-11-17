0e12a2df4cbc29dee908b1716843998615c6086b
Justin Roche <jproche5@gmail.com>
2019-11-17 10:57:55 +0700

    add list of most used keys

--- src/key_metrics/core.clj
+++ src/key_metrics/core.clj
@@ -4,6 +4,7 @@
 
 (def log-path "/Users/justin/logfile.txt")
 (def hour-keys 3500)
+(def day-hours (* hour-keys 8))
 
 (defn get-formatter [s]
   (java.time.format.DateTimeFormatter/ofPattern s))
@@ -33,20 +34,37 @@
 (defn get-hours-worked [day]
   (int (/ (count day) hour-keys)))
 
+(defn get-percent-for-day [d]
+  (int (* 100 (/ (count d) day-hours))))
+
 (defn print-report [days]
-  (let [obj [{:name  "total days" :value (count days)}
-             {:name  "keys today" :value (count (last days))}
-             {:name  "hours today" :value (get-hours-worked (last days))}
-             {:name  "keys this hour" :value (count (last (part-hour (last days))))}]]
-    (pp/print-table obj)))
+  (let [today (last days)
+        table [{:name  "total days" :value (count days)}
+               {:name  "keys today" :value (count today)}
+               {:name  "percent today" :value (get-percent-for-day today)}
+               {:name  "hours today" :value (get-hours-worked today)}
+               {:name  "keys this hour" :value (count (last (part-hour today)))}]]
+    (pp/print-table table)))
+
+(defn get-frequencies [data]
+  (let [table-data (->> (map first data)
+                        frequencies
+                        (sort-by last)
+                        ;; reverse
+                        vec)
+        table (map  (fn [el]
+                      {:key (first el)
+                       :count (second el)}) table-data)]
+    (pp/print-table table)))
 
 (defn get-data []
   (let [data (read-file)
         days  (partition-by #(.getDayOfWeek (:obj (last %)))  data)]
-    (print-report days)))
+    (get-frequencies data)))
 
 (defn set-interval [callback ms]
   (future (while true (do (Thread/sleep ms) (callback)))))
 
 (get-data)
-;; (def job (set-interval get-data 60000))
+
+;; (def job (set-interval get-data 1000))
